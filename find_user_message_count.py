9c024addfeb42056b88942e75ce451f0ed1ce622
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-12 22:04:50 +0500

    This function will find the user's message count.

--- find_user_message_count.py
+++ find_user_message_count.py
@@ -1,5 +1,5 @@
 from read_data import read_data
-from find_all_users_id import find_all_users_id
+from find_all_users_name import find_all_users_name
 
 def find_user_message_count(data: dict, users_id: str)->dict:
     """
@@ -11,4 +11,22 @@ def find_user_message_count(data: dict, users_id: str)->dict:
     Returns:
         dict: Number of messages of the users
     """
-    return
+    count_message = {}
+    for n in users_id:
+        count_message[n]=0
+    for i in data["messages"]:
+        if "actor" in i.keys():
+            if str(type(i['text']))=="<class 'list'>":
+                for n in i['text']:
+                    count_message[i['actor']]+=1
+            else:
+                count_message[i['actor']]+=1
+        else:
+            if str(type(i['text']))=="<class 'list'>":
+                for n in i['text']:
+                    count_message[i['from']]+=1
+            else:
+                count_message[i['from']]+=1
+        
+    return count_message
+print(find_user_message_count(read_data('data/result.json'),find_all_users_name(read_data('data/result.json'))))
